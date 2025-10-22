3c7ba61c93fa6127b6450475900303b064a9518a
compu nueva <justosanchezprogramacion@gmail.com>
2025-10-22 17:32:33 -0300

    Confirmación inicial

---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import { ItemDetail } from "../ItemDetail/ItemDetail";
+
+export const ItemDetailContainer = () => {
+    const [detail, setDetail] = useState({});
+    const { id } = useParams();
+
+    useEffect(()  => {
+       fetch("/data/products.json")       
+        .then((res) => {
+        if (!res.ok){
+            throw new Error("No se encontró el producto");
+        }
+        return res.json();
+
+       })
+       .then((data) => {
+        const found = data.find((prod) => prod.id === id)
+        if(found){
+            setDetail(found);
+        }else{
+             throw new Error("No se encontró el producto");
+        }        
+       })
+       .catch(() => {        
+       })
+    }, [id]);
+    return (
+      <main>
+        {Object.keys(detail).length ?(
+            <ItemDetail detail={detail} />
+        ):(
+            <p>Cargando...</p>
+        )}
+      </main>  
+    );
+};
---
+++ src/context/CartContext/CartProvider.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { CartContext } from "./CartContext";
+export const CartProvider = ({children}) => {
+    const[cart, setCart] = useState([]);
+
+    const exist = (id) => {
+     const exist = cart.some(p => p.id === id)
+     return exist;
+    };
+
+    const addItem =(item) => {
+        if (exist(item.id)){
+            alert("El producto ya existe en el carrito");
+            return;
+        }
+
+
+        setCart([...cart, item]);
+        alert(`${item.name} agregado`);
+    };
+
+    const clearCart = () => {
+        setCart({});
+    };
+
+    const getTotalItems = () => {
+    if (cart.length){
+        return cart.length;
+        }
+    }; 
+
+    const values =  {
+        cart, addItem, clearCart, getTotalItems,
+    };
+
+    return <CartContext.Provider value={values}>{children}</CartContext.Provider>;
+ };
